ddf2a95aef1119f1d3e5a0e7634e037475997b89
Hugo Santos <hdsantos89@gmail.com>
2017-11-21 09:48:57 +1100

    Added TextboxQuestionControl
    We've added a label and a textbox and created it's properties and get-setter

---
+++ SQL Connection/SQL Connection/TextboxQuestionControl.ascx
@@ -0,0 +1,1 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="TextboxQuestionControl.ascx.cs" Inherits="SQL_Connection.TextboxQuestionControl" %>
---
+++ SQL Connection/SQL Connection/TextboxQuestionControl.ascx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SQL_Connection
+{
+    public partial class TextboxQuestionControl : System.Web.UI.UserControl
+    {
+        public Label QuestionLabel
+        {
+            get { return questionLabel; }
+            set { questionLabel = value; }
+        }
+
+        public TextBox QuestionTextBox
+        {
+            get { return questionTextBox; }
+            set { questionTextBox = value; }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
